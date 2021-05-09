6c2d1ad5afbda9686024f21d1b00b6175ac96a69
Alexander <alexander.wirdemo@gmail.com>
2021-05-09 15:09:17 +0200

    First setup

--- src/SeleniumLab.java
+++ src/SeleniumLab.java
@@ -1,2 +1,12 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 public class SeleniumLab {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.findElement(By.id("zV9nZe")).click();
+    }
 }
